c5ede7f190b4b444de0a7de5f8ef9b4945497134
LukeRowberry <luke.rowberry@tooeleschools.org>
2020-10-07 10:08:51 -0600

    updated files from school 10/7/2020

---
+++ hobbies.py
@@ -0,0 +1,12 @@
+hobby1 = "Sleeping"
+hobby2 = "Eating"
+hobby3 = "Spinning in a chair"
+
+myHobbies = [hobby1,
+             hobby2,
+             hobby3]
+print(myHobbies)
+print(myHobbies[1])
+myHobbies[2] = "Guitar"
+print(myHobbies)
+print(myHobbies[0:2])
---
+++ top_games.py
@@ -0,0 +1,120 @@
+game1 = "Team Fortress 2"
+game2 = "Minecraft"
+game3 = "Ocarina of Time"
+game4 = "Half Life 2"
+game5 = "Breath of the Wild" 
+game6 = "Half Life"
+game7 = "Payday2"
+game8 = "Garry's Mod"
+game9 = "Tropico 5"
+game10 = "Red Dead Redemption 2"
+game11 = "Rainbow Six Siege" 
+game12 = "Portal"
+game13 = "CS:GO"
+game14 = "RISK"
+game15 = "Golf With Your Friends"
+game16 = "Fistful of Frags"
+game17 = "UNO"
+game18 = "Last of Us"
+game19 = "Stick Fight:The Game"
+game20 = "Links Awakening Remake"
+game21 = "Terraria"
+game22 = "Among Us"
+game23 = "Mario Kart"
+game24 = "Dota"
+game25 = "Super Smash Bros Ultimate"
+
+top_games = [game1,
+             game2,
+             game3,
+             game4,
+             game5,
+             game6,
+             game7,
+             game8,
+             game9,
+             game10,
+             game11,
+             game12,
+             game13,
+             game14,
+             game15,
+             game16,
+             game17,
+             game18,
+             game19,
+             game20,
+             game21,
+             game22,
+             game23,
+             game24,
+             game25,] #length of list -1
+
+if "World of Warcraft" in top_games:
+    print("pass")
+else:
+    print("fail")
+
+if top_games[0] != "Link To The Past":
+    print("fail")
+else:
+    print("pass")
+
+for i in top_games:
+    if "Pokemon" in i or "Halo" in i:
+        print("fail")
+##top_games[2] = "something else"
+##top_games[1] = "something else"
+##top_games[0] = "something else"
+##top_games.append("new game")
+##print(top_games[25])
+##CLASSLIST = ("Bob","Tim","Frank","Eric") #Cannot be changed
+##print(CLASSLIST)
+
+high_scores = [400,950,375,600,550,200,443,910,395,780]
+##print(len(high_scores))
+##print(high_scores[len(high_scores)-1])
+##print(max(high_scores))
+##high_scores.append(200)
+##print(min(high_scores))
+##high_scores.insert(0,20000)
+##print(max(high_scores))
+
+##high_scores.remove(20000)
+##high_scores.remove(high_scores[0])
+##top_score = high_scores.pop(0)
+##top_score = high_scores.pop(0)
+##top_score = high_scores.pop(0)
+##top_score = high_scores.pop(0)
+##top_score = high_scores.pop(0)
+##print(top_score)
+##print(max(high_scores))
+##high_scores.clear()
+##print(len(high_scores))
+
+##print(top_games.index("Minecraft"))
+##top_games.append("Minecraft")
+##top_games.append("Minecraft")
+##top_games.append("Minecraft")
+##top_games.count("Minecraft")
+##print(top_games.count("Minecraft"))
+##
+##
+##print(high_scores)
+##high_scores.sort()
+##
+##print(high_scores)
+##
+##high_scores.reverse()
+##print(high_scores)
+
+
+
+
+
+
+
+
+
+
+
