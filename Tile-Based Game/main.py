96403855b5481040687bee2c00758fc082a53c01
LukeRowberry <luke.rowberry@tooeleschools.org>
2021-04-19 10:06:51 -0600

    Tile-Based Game : Resume on Part-8

--- Tile-Based Game/main.py
+++ Tile-Based Game/main.py
@@ -3,6 +3,7 @@ import sys
 from os import path
 from settings import *
 from sprites import *
+from tilemap import *
 
 class Game:
     def __init__(self):
@@ -15,21 +16,27 @@ class Game:
 
     def load_data(self):
         game_folder = path.dirname(__file__)
-        self.map_data = []
-        with open(path.join(game_folder,"map.txt"), "rt") as f:
-            for line in f:
-                self.map_data.append(line)
+        img_folder = path.join(game_folder, "images")
+        self.map = Map(path.join(game_folder,"map.txt"))
+        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        for row, tiles in enumerate(self.map_data):
+        self.mobs = pg.sprite.Group()
+        for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
                 if tile == "1":
                     Wall(self, col, row)
+                if tile == "M":
+                    Mob(self, col, row)
                 if tile == "P":
                     self.player = Player(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
 
     def run(self):
         # game loop - set self.playing = False to end the game
@@ -47,6 +54,7 @@ class Game:
     def update(self):
         # update portion of the game loop
         self.all_sprites.update()
+        self.camera.update(self.player)
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
@@ -56,8 +64,15 @@ class Game:
 
     def draw(self):
         self.screen.fill(BG_COLOR)
-        self.draw_grid()
-        self.all_sprites.draw(self.screen)
+        #self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+
+        #DEV NOTES ----------
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        #pg.draw.rect(self.screen, WHITE, self.camera.apply(self.player), 2)
+        #DEV NOTES ----------
+
         pg.display.flip()
 
     def events(self):
--- Tile-Based Game/settings.py
+++ Tile-Based Game/settings.py
@@ -1,6 +1,4 @@
 import pygame as pg
-import random
-import os
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -12,6 +10,7 @@ PURPLE = (75, 0, 130)
 ORANGE = (255, 69, 0)
 PINK = (254, 127, 156)
 BROWN = (139, 69, 19)
+BROWN2 = (106, 55, 5)
 C_BLUE = (100, 149, 237)
 DARKGREY = (40, 40, 40)
 LIGHTGREY = (100, 100, 100)
@@ -21,11 +20,19 @@ HEIGHT = 768
 WIDTH = 1024
 FPS = 60
 TITLE = "Tile_Based Demo"
-BG_COLOR = DARKGREY
+BG_COLOR = BROWN
 
-TILESIZE = 32
+TILESIZE = 64
 GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
+WALL_IMG = "tileGreen_38.png"
+
 #Player Settings
 PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 250
+PLAYER_IMG = "manBlue_gun.png"
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+
+#Mob Settings
+MOB_IMG = "zoimbie1_hold.png"
